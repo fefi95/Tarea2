e049e8803428cfe39ad6b9716365cad930322dd8
Stefani Castellanos <scct95@hotmail.com>
2015-04-21 17:11:58 -0430

    Tarea2v2

---
+++ calcularPrecio/calcularPrecioTester.py
@@ -0,0 +1,18 @@
+'''
+Created on 21/04/2015
+
+@author: stefani
+'''
+import unittest
+
+#Pruebas de la funcion calcularPrecio
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
