dde0740e7869954750df9c46d6a14bb833e03ed2
Stefani Castellanos <scct95@hotmail.com>
2015-04-22 15:43:17 -0430

    Tarea2v2 Stefani

--- calcularPrecio/calcularPrecioTester.py
+++ calcularPrecio/calcularPrecioTester.py
@@ -8,12 +8,12 @@ import calcularPrecio
 from datetime import datetime
 
 #Pruebas de la funcion calcularPrecio
-class Test(unittest.TestCase):
+class TestcalcularPrecio(unittest.TestCase):
 
     def testminTarifaDiaSemana(self):
         diasem = calcularPrecio.Tarifa(1, 2) 
         tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 22, 6, 50)]
-        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 1.00, "Yei")
+        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 1.00)
 
 
 
