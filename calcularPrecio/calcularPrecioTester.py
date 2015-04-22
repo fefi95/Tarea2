794ea980cde0fcefbca661dedc055f7a00a0ccb9
Stefani Castellanos <scct95@hotmail.com>
2015-04-22 16:58:56 -0430

    Tareav2.1 (Modificado tester)

--- calcularPrecio/calcularPrecioTester.py
+++ calcularPrecio/calcularPrecioTester.py
@@ -6,15 +6,19 @@ Created on 21/04/2015
 import unittest
 import calcularPrecio
 from datetime import datetime
-
+from decimal import Decimal
 #Pruebas de la funcion calcularPrecio
 class TestcalcularPrecio(unittest.TestCase):
 
     def testminTarifaDiaSemana(self):
         diasem = calcularPrecio.Tarifa(1, 2) 
         tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 22, 6, 50)]
-        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 1.00)
+        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), Decimal(1.00).quantize(Decimal('1.00')))
 
+    def testminReservaPosible(self):
+        diasem = calcularPrecio.Tarifa(2, 2) 
+        tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 22, 6, 06)]
+        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), Decimal(0.53).quantize(Decimal('1.00')))
 
 
 if __name__ == "__main__":
