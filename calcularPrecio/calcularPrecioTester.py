143e562da22ae9a2c048469faf096804ef87dfe8
edwin <e.murillo0408@gmail.com>
2015-04-23 16:28:00 -0430

    Ultimo Commit de Tests

--- calcularPrecio/calcularPrecioTester.py
+++ calcularPrecio/calcularPrecioTester.py
@@ -1,42 +1,34 @@
 '''
-Created on 22/04/2015
-
-@author: edwin
+Created on 21/04/2015
+@author: stefani
 '''
 import unittest
 import calcularPrecio
-from datetime import datetime
+from datetime import datetime, MINYEAR, MAXYEAR
+from decimal import Decimal
 
 #Pruebas de la funcion calcularPrecio
-class Test(unittest.TestCase):
+class TestcalcularPrecio(unittest.TestCase):
         
     def testMinTarifaFinDeSemana(self):
-        diasem = calcularPrecio.Tarifa(1, 0) 
+        tarifa = calcularPrecio.Tarifa(1, 0) 
         tiemporeserva = [datetime(2015, 4, 22, 0, 00), datetime(2015, 4, 27, 0, 4)]
-        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 73.00)
+        self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(73).quantize(Decimal('1.00')))
 
     def testMinTarifaDiaSemana(self):
-        diasem = calcularPrecio.Tarifa(0, 1) 
+        tarifa = calcularPrecio.Tarifa(0, 1) 
         tiemporeserva = [datetime(2015, 4, 22, 0, 00), datetime(2015, 4, 27, 0, 00)]
-        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 48.00)
-        
-    def testMaxTarifaDiaSemana(self):
-        diasem = calcularPrecio.Tarifa(2**50, 0) 
-        tiemporeserva = [datetime(2015, 4, 22, 0, 00), datetime(2015, 4, 22, 1, 00)]
-        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 2**50)
-        
-    def testMaxTarifaFinDeSemana(self):
-        diasem = calcularPrecio.Tarifa(0, 9223372036854775807) 
-        tiemporeserva = [datetime(2015, 4, 25, 0, 00), datetime(2015, 4, 25, 1, 00)]
-        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), 9223372036854775807)
+        self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(48).quantize(Decimal('1.00')))
         
     def testExceptionTarifaDiaSemana(self):
-        diasem = calcularPrecio.Tarifa(-1, -1) 
+        tarifa = calcularPrecio.Tarifa(-0.01, 0) 
         tiemporeserva = [datetime(2015, 4, 22, 0, 00), datetime(2015, 4, 22, 1, 00)]
-        with self.assertRaises(Exception) as context:
-            calcularPrecio.calcularPrecio(diasem, tiemporeserva)
-
-        self.assertTrue("No se admiten tarifas negativas." in context.exception)
+        self.assertRaises(Exception, calcularPrecio, tarifa, tiemporeserva)
+        
+    def testExceptionTarifaFinDeSemana(self):
+        tarifa = calcularPrecio.Tarifa(0, -0.01) 
+        tiemporeserva = [datetime(2015, 4, 22, 0, 00), datetime(2015, 4, 22, 1, 00)]
+        self.assertRaises(Exception, calcularPrecio, tarifa, tiemporeserva)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
