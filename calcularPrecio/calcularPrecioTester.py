f9b5cbb3018b638b27fd41f02a382ba095d5e44d
Stefani Castellanos <scct95@hotmail.com>
2015-04-23 11:53:23 -0430

    Tarea2v2.4 (Modificado tester)

--- calcularPrecio/calcularPrecioTester.py
+++ calcularPrecio/calcularPrecioTester.py
@@ -7,6 +7,7 @@ import unittest
 import calcularPrecio
 from datetime import datetime, MINYEAR, MAXYEAR
 from decimal import Decimal
+
 #Pruebas de la funcion calcularPrecio
 class TestcalcularPrecio(unittest.TestCase):
 
@@ -17,27 +18,27 @@ class TestcalcularPrecio(unittest.TestCase):
 
     def testminReservaPosible(self):
         tarifa = calcularPrecio.Tarifa(2, 3) 
-        tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 22, 6, 05)]
+        tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 22, 6, 5)]
         self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(2.00).quantize(Decimal('1.00')))
     
     def testmaxReservaPosible(self):
         tarifa = calcularPrecio.Tarifa(2, 3) 
-        tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 29, 5, 49)]
+        tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 29, 5, 50)]
         self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(384).quantize(Decimal('1.00')))
     
     def testmaxfechaPosible(self):
         tarifa = calcularPrecio.Tarifa(2, 3) 
-        tiemporeserva = [datetime(MAXYEAR, 4, 22, 5, 50), datetime(MAXYEAR, 4, 29, 5, 49)]
+        tiemporeserva = [datetime(MAXYEAR, 12, 24, 5, 50), datetime(MAXYEAR, 12, 31, 5, 49)]
         self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(384).quantize(Decimal('1.00')))
     
     def testminfechaPosible(self):
         tarifa = calcularPrecio.Tarifa(2, 3) 
-        tiemporeserva = [datetime(MINYEAR, 4, 22, 5, 50), datetime(MINYEAR, 4, 29, 5, 49)]
+        tiemporeserva = [datetime(MINYEAR, 1, 1, 5, 50), datetime(MINYEAR, 1, 8, 5, 49)]
         self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(384).quantize(Decimal('1.00')))
     
     def testReservaMenor15(self):
         tarifa = calcularPrecio.Tarifa(2, 3) 
-        tiemporeserva = [datetime(MINYEAR, 4, 22, 5, 50), datetime(MINYEAR, 4, 22, 6, 04)]
+        tiemporeserva = [datetime(MINYEAR, 4, 22, 5, 50), datetime(MINYEAR, 4, 22, 6, 4)]
         with self.assertRaises(Exception) as context:
             calcularPrecio.calcularPrecio(tarifa, tiemporeserva)
 
@@ -51,7 +52,17 @@ class TestcalcularPrecio(unittest.TestCase):
 
         self.assertTrue("La reserva no debe ser mayor a siete (7) dias." in context.exception)
 
-    
+    def testMaxTarifaMinReserva(self):
+        tarifa = calcularPrecio.Tarifa(2**28, 3**27) 
+        tiemporeserva = [datetime(2004, 4, 22, 5, 50), datetime(2004, 4, 22, 6, 5)]
+        self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal(2**28).quantize(Decimal('1.00')))
+
+    def testMaxTarifaMaxReserva(self):
+        tarifa = calcularPrecio.Tarifa(2**28, 3**27) 
+        tiemporeserva = [datetime(2004, 4, 5, 5, 50), datetime(2004, 4, 12, 5, 50)]
+        self.assertEquals(calcularPrecio.calcularPrecio(tarifa, tiemporeserva), Decimal((2**28)*24*5+(3**27*24*2)).quantize(Decimal('1.00')))
+
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
