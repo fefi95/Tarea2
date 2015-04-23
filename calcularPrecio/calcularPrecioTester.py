5ee1964ef756fdabdf316eb0a629f8cdc16dc51a
Stefani Castellanos <scct95@hotmail.com>
2015-04-22 19:50:52 -0430

    Tarea2v2.2 (Modificacion tester)

--- calcularPrecio/calcularPrecioTester.py
+++ calcularPrecio/calcularPrecioTester.py
@@ -5,7 +5,7 @@ Created on 21/04/2015
 '''
 import unittest
 import calcularPrecio
-from datetime import datetime
+from datetime import datetime, MINYEAR, MAXYEAR
 from decimal import Decimal
 #Pruebas de la funcion calcularPrecio
 class TestcalcularPrecio(unittest.TestCase):
@@ -25,7 +25,11 @@ class TestcalcularPrecio(unittest.TestCase):
         tiemporeserva = [datetime(2015, 4, 22, 5, 50), datetime(2015, 4, 29, 5, 49)]
         self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), Decimal(384).quantize(Decimal('1.00')))
     
-
+    def testmaxfechaPosible(self):
+        diasem = calcularPrecio.Tarifa(2, 3) 
+        tiemporeserva = [datetime(MAXYEAR, 4, 22, 5, 50), datetime(MAXYEAR, 4, 29, 5, 49)]
+        self.assertEquals(calcularPrecio.calcularPrecio(diasem, tiemporeserva), Decimal(384).quantize(Decimal('1.00')))
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
